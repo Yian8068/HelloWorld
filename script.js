80d656d13e1167771f1665239366a4c408467b32
Yian <yian8068@yahoo.com.tw>
2015-01-18 00:28:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ $(document).ready(
     $("#div2").toggle(1000);
   });
   $("#div1").mouseenter(function(){
-    $("div3").fadeto('fast',0.5);
+    $("#div3").fadeto('fast',0.5);
     
   });
   $("#div1").mouseleave(function(){
-    $("div3").fadeto('fast',1);
+    $("#div3").fadeto('fast',1);
     
   });
 );
